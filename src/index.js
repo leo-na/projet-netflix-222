76f466ee003b0fec0262143723d7b61f660ff204
leo-na <bekleyleo@gmail.com>
2022-05-05 12:03:59 +0200

    modif

--- src/index.js
+++ src/index.js
@@ -1,88 +1,143 @@
-/*let $form = document.querySelector("#form");
 let $input = document.querySelector(".input");
-let $button = document.querySelector("search-btn3");
-let $count = document.querySelector("#count");
-let $movie = document.querySelector("#movie");
+let $form = document.querySelector(".form");
+let $button = document.querySelector(".search-btn3");
+let api_key = "a4880995fbb07d40249df7d0c03c8383";
+let api_url = "https://api.themoviedb.org/3";
+let $article = document.querySelector(".article");
+let $movie = document.querySelector(".form-select");
+let $secarticle = document.querySelector(".second_article");
+let $secform = document.querySelector(".formulaire");
+let $searchbar = document.querySelector(".searchbar");
+let $buton = document.querySelector(".search-btn2");
 
-function getSearch(query) {
-  console.log(query);
-}
+let page = 1;
 
-$form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  getSearch($input.value);
-  search($input.value);
-});
-
-let search = function () {
-  fetch(
-    "https://api.themoviedb.org/3/search/movie?api_key=a4880995fbb07d40249df7d0c03c8383&query=" +
-      $input.value
-  )
+let firstfunction = function () {
+  fetch(api_url + "/search/movie?api_key=" + api_key + "&query=" + $input.value)
     .then(function (response) {
+      console.log("fetch finished");
+
+      if (response.status === 404) {
+        alert("pas de film");
+        return;
+      }
       return response.json();
     })
-    .then(function (film) {
-      console.log(film);
+    .then(function (response) {
+      if (response == undefined) {
+        return;
+      }
+      if (response.results.length === 0) {
+        alert("il n'y a pas de film pour cette recherche");
+        return;
+      }
+      console.log(response);
+
+      let film = response.results[0];
+
+      let $h2 = document.createElement("h2");
+      $h2.textContent = film.title;
+
+      let $text = document.createElement("div");
+      $text.textContent = film.overview;
+
+      let $image = document.createElement("img");
+      $image.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + film.poster_path
+      );
+
+      let $img = document.createElement("img");
+      $img.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + film.backdrop_path
+      );
+
+      let $images = document.createElement("div");
+      $images.appendChild($img);
+      $images.appendChild($image);
+
+      $images.style.display = "flex";
+      $images.style.justifyContent = "space-around";
+
+      $article.textContent = "";
+      $article.appendChild($h2);
+      $article.appendChild($text);
+      $article.appendChild($images);
+
+      $article.style.color = "white";
+      /*  $article.style.display = "flex";
+      $article.style.justify - content, "space-betwen";*/
     });
-};*/
+};
 
-const API_URL = "https://api.themoviedb.org/3";
-const API_KEY = "a4880995fbb07d40249df7d0c03c8383";
-
-const $form = document.querySelector("#form");
-const $input = document.querySelector("#search");
-const $count = document.querySelector("#count");
-const $results = document.querySelector("#results");
-const $movie = document.querySelector("#movie");
-
-// Make an API request to get movies for the given query
-function getMovies(query) {
-  fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${query}`)
-    .then((response) => response.json())
-    .then((response) => showMovies(response));
-}
-
-function getMoviesDEtails(movie) {
-  fetch();
-}
-
-// Add the movies buttons to the DOM
-function showMovies(movies) {
-  $results.innerHTML = "";
-
-  if (movies.total_results === 0) {
-    $count.innerText = "Aucun résultat, désolé !";
-    return;
-  }
-
-  $count.innerText = `${movies.total_results} résultats`;
-
-  $list = document.createElement("div");
-
-  movies.results.forEach(function (movie) {
-    const $movieButton = document.createElement("button");
-    $movieButton.innerText = movie.title;
-
-    if (movie.release_date) {
-      const year = movie.release_date.split("-")[0];
-      // alternative : const year = new Date(movie.release_date).getFullYear();
-      $movieButton.innerText += ` (${year})`;
-    }
-
-    $movieButton.addEventListener("click", function () {
-      console.log(movie);
-    });
-
-    $list.appendChild($movieButton);
-  });
-
-  $results.appendChild($list);
-}
-
-// Listen to form submit to trigger search
-$form.addEventListener("submit", function (event) {
+$button.addEventListener("click", function (event) {
   event.preventDefault();
-
-  getMovies($input.value);
+  console.log("clicked");
+  firstfunction();
 });
+
+let secondfunction = function () {
+  fetch(
+    api_url + "/search/person?query=" + $searchbar.value + "&api_key=" + api_key
+  )
+    .then((respone) => respone.json())
+    .then((respone) => {
+      console.log(respone);
+
+      for (let i = 0; i < respone.results.length; i++) {
+        console.log(i);
+      }
+      let actor = respone.results[0];
+
+      let $h2bis = document.createElement("h2");
+      $h2bis.textContent = actor.name;
+
+      let $addtext = document.createElement("p");
+      $addtext.textContent = actor.known_for_department;
+
+      let $textbis = document.createElement("div");
+      $textbis.textContent = actor.known_for[1].overview;
+
+      /* let $imagebis = document.createElement("img");
+      $imagebis.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + actor.poster_path
+      );*/
+
+      let $imgbis = document.createElement("img");
+      $imgbis.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + actor.profile_path
+      );
+
+      let $imagesbis = document.createElement("div");
+      // $imagesbis.appendChild($imagebis);
+      $imagesbis.appendChild($imgbis);
+
+      $imagesbis.style.display = "flex";
+      $imagesbis.style.justifyContent = "space-around";
+
+      $secarticle.textContent = "";
+      $secarticle.appendChild($h2bis);
+      $secarticle.appendChild($textbis);
+      $secarticle.appendChild($addtext);
+      $secarticle.appendChild($imagesbis);
+
+      $secarticle.style.color = "white";
+    });
+};
+
+$buton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("clicked");
+  secondfunction();
+});
+
+let thirdfunction = function () {
+  fetch(api_url + "/genre/tv/list" + "craig" + "&api_key=" + api_key)
+    .then((anwser) => anwser.json())
+    .then((anwser) => {
+      console.log(anwser);
+    });
+};
